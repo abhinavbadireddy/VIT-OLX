5d1226060e26b251c01216bb7fde6f404155c3ae
Anishsme <anishsme@gmail.com>
2022-06-23 09:50:34 +0530

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/index.html", "./dist/logged_in_home.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
